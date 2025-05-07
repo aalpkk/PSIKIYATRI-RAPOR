8bab9b77541c574babe663eef4e3716150d018ae
aalpkk <alpkarakasli@gmail.com>
2025-05-07 15:46:01 +0300

    2_TMK_432_Yatis.py

--- pages/2_TMK_432_Yatis.py
+++ pages/2_TMK_432_Yatis.py
@@ -24,24 +24,27 @@ karar = st.radio("Rapor Sonucu", [
 # ↓ Ruhsal durum muayenesi bileşenleri
 st.markdown("### Ruhsal Durum Muayenesi")
 duygudurum = st.selectbox("Duygudurum", ["çökkün", "disforik", "anksiyöz", "öforik", "coşkulu", "taşkın", "ötimik"])
-duygulanim = st.selectbox("Duygulanım", ["uygun", "uygunsuz", "düzleşmiş", "küntleşmiş", "sığ", "çökkünlük yönünde artmış", "taşkınlık yönünde artmış"])
-cagrisimlar = st.multiselect("Çağrışım Özellikleri", ["çağrışımlarının düzenli", "çağrışımlarının hızlanmış", "çağrışımlarının yavaşlamış", "çağrışımlarının gevşemiş", "çağrışımlarının teğetsel", "çağrışımlarının çevresel", "düşünce sürecinde enkoherans", "basınçlı konuşmasının", "fikir uçuşmalarının", "clang çağrışımlarının", "düşünce sürecinde perseverasyonların", "düşünce sürecinde blokların"])
+duygulanim = st.selectbox("Duygulanım", [
+    "uygun", "uygunsuz", "düzleşmiş", "küntleşmiş", "sığ", 
+    "çökkünlük yönünde artmış", "taşkınlık yönünde artmış"
+])
+cagrisimlar = st.multiselect("Çağrışım Özellikleri", [
+    "çağrışımlarının düzenli", "çağrışımlarının hızlanmış", "çağrışımlarının yavaşlamış",
+    "çağrışımlarının gevşemiş", "çağrışımlarının teğetsel", "çağrışımlarının çevresel",
+    "düşünce sürecinde enkoherans", "basınçlı konuşmasının", "fikir uçuşmalarının",
+    "clang çağrışımlarının", "düşünce sürecinde perseverasyonların", "düşünce sürecinde blokların"
+])
 sanrilar = st.multiselect("Sanrılar", [
     "büyüklük", "kötülük görme", "alınma", "düşünce sokulması", "düşünce okunması",
-    "düşünce çalınması", "düşünce yayınlanması", "somatik edilginlik", 
-"duygudulanımın edilginliği",
-    "dürtünün edilginliği", "iradenin edilginliği", "erotomanik", 
-"küçüklük", "nihilistik",
+    "düşünce çalınması", "düşünce yayınlanması", "somatik edilginlik", "duygudulanımın edilginliği",
+    "dürtünün edilginliği", "iradenin edilginliği", "erotomanik", "küçüklük", "nihilistik",
     "sadakatsizlik", "somatik", "hipokondriak", "enfestasyon"
 ])
 varsanilar = st.multiselect("Varsanılar", ["işitme", "görme", "taktil"])
-zeka = st.selectbox("Zeka (Soyutlama)", ["soyutlamasının bozuk olduğu", 
-"soyutlamasının olağan olduğu"])
+zeka = st.selectbox("Zeka (Soyutlama)", ["soyutlamasının bozuk olduğu", "soyutlamasının olağan olduğu"])
 
 # ↓ Ruhsal durum metni oluştur
-mse = f"İlgilinin {rapor_tarihi.strftime('%d/%m/%Y')} tarihindeki ruhsal 
-durum muayenesinde duygudurumunun {duygudurum}, duygulanımının 
-{duygulanim} olduğu"
+mse = f"İlgilinin {rapor_tarihi.strftime('%d/%m/%Y')} tarihindeki ruhsal durum muayenesinde duygudurumunun {duygudurum}, duygulanımının {duygulanim} olduğu"
 
 if cagrisimlar:
     mse += ", " + ", ".join(cagrisimlar) + " olduğu"
@@ -51,11 +54,9 @@ else:
 if sanrilar and varsanilar:
     mse += ", " + ", ".join(sanrilar) + " sanrılarının ve " + ", ".join(varsanilar) + " varsanılarının olduğu"
 elif sanrilar and not varsanilar:
-    mse += ", " + ", ".join(sanrilar) + " sanrılarının olduğu, 
-algılamasının doğal olduğu"
+    mse += ", " + ", ".join(sanrilar) + " sanrılarının olduğu, algılamasının doğal olduğu"
 elif not sanrilar and varsanilar:
-    mse += ", " + ", ".join(varsanilar) + " varsanılarının olduğu, düşünce 
-içeriğinin sanrısal içeriğe sahip olmadığı"
+    mse += ", " + ", ".join(varsanilar) + " varsanılarının olduğu, düşünce içeriğinin sanrısal içeriğe sahip olmadığı"
 else:
     mse += ", sanrı ve varsanısının olmadığı"
 
@@ -63,46 +64,23 @@ mse += f", {zeka} tespit edilmiştir."
 
 # ↓ Sonuç metni seçimi
 if karar == "Yatış gerekli (kendisi ve toplum için tehlike)":
-    sonuc = f"İlgiliye {tani} tanısı koyulmuş olup, kişinin söz konusu 
-akıl hastalığı nedeniyle kendisi ve toplum açısından tehlikelilik 
-oluşacağından TMK'nin 432. maddesi uyarınca tıbbi durumu elverir vermez 
-çıkarılmak üzere kapalı psikiyatri servisi bulunan bir sağlık kurumunda 
-yatarak tedavi görmesinin uygun olduğunu bildirir sağlık kurulu 
-raporudur."
+    sonuc = f"İlgiliye {tani} tanısı koyulmuş olup, kişinin söz konusu akıl hastalığı nedeniyle kendisi ve toplum açısından tehlikelilik oluşacağından TMK'nin 432. maddesi uyarınca tıbbi durumu elverir vermez çıkarılmak üzere kapalı psikiyatri servisi bulunan bir sağlık kurumunda yatarak tedavi görmesinin uygun olduğunu bildirir sağlık kurulu raporudur."
 
 elif karar == "Herhangi bir bozukluk yok":
-    sonuc = "Alınan öykü, incelenen evrak ve yapılan muayene sonucunda 
-ilgilinin TMK'nın 432. maddesi uyarınca hastanede yatmasını gerektirecek 
-nitelikte herhangi bir akıl hastalığı, akıl zayıflığı, alkol ya da madde 
-bağımlılığı saptanmadığını bildirir rapordur."
+    sonuc = "Alınan öykü, incelenen evrak ve yapılan muayene sonucunda ilgilinin TMK'nın 432. maddesi uyarınca hastanede yatmasını gerektirecek nitelikte herhangi bir akıl hastalığı, akıl zayıflığı, alkol ya da madde bağımlılığı saptanmadığını bildirir rapordur."
 
 elif karar == "Mental retardasyon var, yatış gerekmez":
-    sonuc = "Alınan öykü, incelenen evrak ve yapılan muayene sonucunda 
-ilgiliye mental retardasyon tanısı konulmuş olup bu tanının bir akıl 
-zayıflığı olmakla birlikte ilgilinin halihazırdaki durumuyla toplum için 
-tehlike oluşturmadığı, tedavi, eğitim veya ıslahının mümkün olmadığı, 
-haliyle bir kuruma yerleştirilmesine veya alıkonulmasına gerek olmadığı 
-kanaatini bildirir sağlık kurulu raporudur."
+    sonuc = "Alınan öykü, incelenen evrak ve yapılan muayene sonucunda ilgiliye mental retardasyon tanısı konulmuş olup bu tanının bir akıl zayıflığı olmakla birlikte ilgilinin halihazırdaki durumuyla toplum için tehlike oluşturmadığı, tedavi, eğitim veya ıslahının mümkün olmadığı, haliyle bir kuruma yerleştirilmesine veya alıkonulmasına gerek olmadığı kanaatini bildirir sağlık kurulu raporudur."
 
 else:
-    sonuc = f"Alınan öykü, incelenen evrak ve yapılan muayene sonucunda 
-ilgiliye {tani} tanısı konulmuş olup bu tanının bir akıl hastalığı olmakla 
-birlikte ilgilinin halihazırdaki durumuyla toplum için tehlike 
-oluşturmadığı, tedavi, eğitim veya ıslahı için bir kuruma 
-yerleştirilmesine veya alıkonulmasına gerek olmadığı kanaatini bildirir 
-sağlık kurulu raporudur."
+    sonuc = f"Alınan öykü, incelenen evrak ve yapılan muayene sonucunda ilgiliye {tani} tanısı konulmuş olup bu tanının bir akıl hastalığı olmakla birlikte ilgilinin halihazırdaki durumuyla toplum için tehlike oluşturmadığı, tedavi, eğitim veya ıslahı için bir kuruma yerleştirilmesine veya alıkonulmasına gerek olmadığı kanaatini bildirir sağlık kurulu raporudur."
 
 # Tüm metni birleştir
-tam_rapor = f"""{kurum}un {ust_yazi_tarihi.strftime('%d/%m/%Y')} tarih ve 
-{ust_yazi_sayisi} sayılı yazısı ile TMK’nın 432. maddesi uyarınca hakkında 
-rapor düzenlenmesi için yönlendirilen {tc} T.C. kimlik nolu {ad_soyad}, 
-{rapor_tarihi.strftime('%d/%m/%Y')} tarihinde Hitit Üniversitesi Erol 
-Olçok Eğitim ve Araştırma Hastanesi Psikiyatri Heyetinde 
-değerlendirilmiştir.
-İlgilinin kendisinden ve ilgili tıbbi/adli evraktan edinilen bilgilere 
-göre {anamnez} anlaşılmıştır.
+tam_rapor = f"""{kurum}un {ust_yazi_tarihi.strftime('%d/%m/%Y')} tarih ve {ust_yazi_sayisi} sayılı yazısı ile TMK’nın 432. maddesi uyarınca hakkında rapor düzenlenmesi için yönlendirilen {tc} T.C. kimlik nolu {ad_soyad}, {rapor_tarihi.strftime('%d/%m/%Y')} tarihinde Hitit Üniversitesi Erol Olçok Eğitim ve Araştırma Hastanesi Psikiyatri Heyetinde değerlendirilmiştir.
+İlgilinin kendisinden ve ilgili tıbbi/adli evraktan edinilen bilgilere göre {anamnez} anlaşılmıştır.
 {mse}
-\n{sonuc}"""
 
+{sonuc}"""
+
+# Sonuç gösterimi
 st.text_area("Oluşturulan Rapor", tam_rapor, height=600)
-
